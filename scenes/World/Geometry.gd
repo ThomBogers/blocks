a277294224ea93dfa0f8a04df188a01e6badd468
Thom Bogers <Thom.bogers@gmail.com>
2018-02-24 13:18:22 +0100

    Working tunneling

--- scenes/World/Geometry.gd
+++ scenes/World/Geometry.gd
@@ -44,14 +44,14 @@ func _build_chunk():
 		chunk.append([])
 		for z in range(chunksize):
 			chunk[x].append([])
-			var y_top = randi()%chunksize
+			var y_top = randi()%5
 			for y in range(chunksize):
 				if y <= y_top:
 					chunk[x][z].append(BLOCK_TYPE.DIRT)
 				else:
 					chunk[x][z].append(BLOCK_TYPE.AIR)
 
-func _build_chunk_test():
+func _build_chunk_test0():
 	chunksize = 10
 
 	for x in range(chunksize):
@@ -69,14 +69,56 @@ func _build_chunk_test():
 
 				chunk[x][z].append(current)
 
-	var z = 4
 	var y = 2
 	for x in range(chunksize):
-		chunk[x][z][y] = BLOCK_TYPE.AIR
+		for z in range(4,8):
+			chunk[x][z][y] = BLOCK_TYPE.AIR
+			chunk[x][z][y+1] = BLOCK_TYPE.AIR
+		
+func _build_chunk_test1():
+	chunksize = 3
+
+	for x in range(chunksize*3):
+		chunk.append([])
+		for z in range(chunksize):
+			chunk[x].append([])
+			
+			var h = 0
+			if z == 1 and x%3 == 1:
+				h = 2
+
+			for y in range(chunksize):
+				
+				var current
+				if y <= h:
+					current = BLOCK_TYPE.DIRT
+				else:
+					current = BLOCK_TYPE.AIR
+
+				chunk[x][z].append(current)
+
+		
+func _build_chunk_test2():
+	chunksize = 1
+
+	for x in range(chunksize):
+		chunk.append([])
+		for z in range(chunksize):
+			chunk[x].append([])
+			
+			for y in range(chunksize):
+				
+				var current
+				if y == 0:
+					current = BLOCK_TYPE.DIRT
+				else:
+					current = BLOCK_TYPE.AIR
+
+				chunk[x][z].append(current)
 
 func _ready():
 	#_build_chunk()
-	_build_chunk_test()
+	_build_chunk_test0()
 
 	meshInstance.set_material_override(material)
 
@@ -85,12 +127,13 @@ func _ready():
 
 	surfTool.begin(Mesh.PRIMITIVE_TRIANGLES)
 
-	for x in range(chunk.size() - 1):
+	for x in range(0,chunk.size()):
 		xoffset = x*cubesize
-		for z in range(chunk[x].size() - 1):
+		for z in range(0,chunk[x].size()):
 			zoffset = z*cubesize
 
-			for y in range(0, (chunk[x][z].size() )):
+			for y in range(0,chunk[x][z].size()):
+				
 				yoffset = y*cubesize
 				var res
 				res = _get_vertical_z(x,z,y)
@@ -137,21 +180,18 @@ func _get_random_y(x,z):
 		down   = randi()%10
 
 	var res = prev + up - down
-	print("PREV :", prev, "RES : ", res)
 
 	return res
 
 func _get_horizontal(x, z, y):
 	var current_type  = chunk[x][z][y]
-
+	
 	var next_type
-	if y == (chunk[x][z].size() - 1):
+	if y >= (chunk[x][z].size() - 1):
 		next_type = BLOCK_TYPE.AIR
 	else:
 		next_type = chunk[x][z][y+1]
 
-
-	#print("x: ", x, " z: ", z, " y: ", y, " tc: ", current_type, " tn ", next_type)
 
 	if current_type == next_type:
 		return null
@@ -198,20 +238,13 @@ func _get_horizontal(x, z, y):
 	return [varray, uvarray, carray]
 
 func _get_vertical_x(x,z,y):
-	if x == 0:
-		#Should check previous chunk
-		return null
-
 	var current_type  = chunk[x][z][y]
-
+	
 	var next_type
-	if x == (chunk.size() - 1):
+	if x >= (chunk.size() - 1):
 		next_type = BLOCK_TYPE.AIR
 	else:
 		next_type = chunk[x+1][z][y]
-
-
-	#print("x: ", x, " z: ", z, " y: ", y, " tc: ", current_type, " tn ", next_type)
 
 	if current_type == next_type:
 		return null
@@ -228,21 +261,20 @@ func _get_vertical_x(x,z,y):
 	var yoff_top = (y) * cubesize
 	var yoff_bot = (y-1)*cubesize
 
-
 	uv1 = Vector2(0,0)
-	v1 = Vector3(xoffset,yoff_top,zoffset)
+	v1 = Vector3(xoffset+cubesize,yoff_top,zoffset)
 	c1 = Color(0,0,0.5)
 
 	uv2 = Vector2(0,1)
-	v2 = Vector3(xoffset,yoff_top,zoffset+cubesize)
+	v2 = Vector3(xoffset+cubesize,yoff_top,zoffset+cubesize)
 	c2 = Color(0,0,0.5)
 
 	uv3 = Vector2(1,1)
-	v3 = Vector3(xoffset,yoff_bot,zoffset+cubesize)
+	v3 = Vector3(xoffset+cubesize,yoff_bot,zoffset+cubesize)
 	c3 = Color(0,0,0.5)
 
 	uv4 = Vector2(1,0)
-	v4 = Vector3(xoffset,yoff_bot,zoffset)
+	v4 = Vector3(xoffset+cubesize,yoff_bot,zoffset)
 	c4 = Color(0,0,0.5)
 
 	uvarray.append(uv4)
@@ -263,20 +295,14 @@ func _get_vertical_x(x,z,y):
 	return [varray, uvarray, carray]
 
 func _get_vertical_z(x,z,y):
-	if z == 0:
-		#Should check previous chunk
-		return null
 
 	var current_type  = chunk[x][z][y]
-
+	
 	var next_type
-	if x == (chunk.size() - 1):
+	if z >= (chunk[x].size() -1):
 		next_type = BLOCK_TYPE.AIR
 	else:
 		next_type = chunk[x][z+1][y]
-
-
-	#print("x: ", x, " z: ", z, " y: ", y, " tc: ", current_type, " tn ", next_type)
 
 	if current_type == next_type:
 		return null
@@ -295,19 +321,19 @@ func _get_vertical_z(x,z,y):
 
 
 	uv1 = Vector2(0,0)
-	v1 = Vector3(xoffset,yoff_top,zoffset)
+	v1 = Vector3(xoffset,yoff_top,zoffset+cubesize)
 	c1 = Color(0,0,0.5)
 
 	uv2 = Vector2(0,1)
-	v2 = Vector3(xoffset+cubesize,yoff_top,zoffset)
+	v2 = Vector3(xoffset+cubesize,yoff_top,zoffset+cubesize)
 	c2 = Color(0,0,0.5)
 
 	uv3 = Vector2(1,1)
-	v3 = Vector3(xoffset+cubesize,yoff_bot,zoffset)
+	v3 = Vector3(xoffset+cubesize,yoff_bot,zoffset+cubesize)
 	c3 = Color(0,0,0.5)
 
 	uv4 = Vector2(1,0)
-	v4 = Vector3(xoffset,yoff_bot,zoffset)
+	v4 = Vector3(xoffset,yoff_bot,zoffset+cubesize)
 	c4 = Color(0,0,0.5)
 
 	uvarray.append(uv4)
